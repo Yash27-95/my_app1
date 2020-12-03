f53cb662cc1a928dae0f75a231fb38ce6b12f468
Yash27-95 <70720726+Yash27-95@users.noreply.github.com>
2020-12-03 18:09:46 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,142 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input
+          id="input-2"
+          v-model="newTask"
+          required
+          placeholder="Enter Task"
+          @keyup.enter="add"
+        ></b-form-input>
+        <b-button @click="add" variant="primary" class="ml-3">Add</b-button>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>Back Log</h3>
+          <!-- Backlog draggable component. Pass arrBackLog to list prop -->
+          <draggable
+            class="list-group kanban-column"
+            :list="arrBackLog"
+            group="tasks"
+          >
+            <div
+              class="list-group-item"
+              v-for="element in arrBackLog"
+              :key="element.name"
+            >
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
       </div>
 
-      <v-spacer></v-spacer>
+      <div class="col-3">
+        <div class="p-2 alert alert-primary">
+          <h3>In Progress</h3>
+          <!-- In Progress draggable component. Pass arrInProgress to list prop -->
+          <draggable
+            class="list-group kanban-column"
+            :list="arrInProgress"
+            group="tasks"
+          >
+            <div
+              class="list-group-item"
+              v-for="element in arrInProgress"
+              :key="element.name"
+            >
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <div class="col-3">
+        <div class="p-2 alert alert-warning">
+          <h3>Testing</h3>
+          <!-- Testing draggable component. Pass arrTested to list prop -->
+          <draggable
+            class="list-group kanban-column"
+            :list="arrTested"
+            group="tasks"
+          >
+            <div
+              class="list-group-item"
+              v-for="element in arrTested"
+              :key="element.name"
+            >
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      <div class="col-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <!-- Done draggable component. Pass arrDone to list prop -->
+          <draggable
+            class="list-group kanban-column"
+            :list="arrDone"
+            group="tasks"
+          >
+            <div
+              class="list-group-item"
+              v-for="element in arrDone"
+              :key="element.name"
+            >
+              {{ element.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import draggable
+import draggable from "vuedraggable";
 
 export default {
-  name: 'App',
-
+  name: "kanban-board",
   components: {
-    HelloWorld,
+    //import draggable as a component
+    draggable
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      // for new tasks
+      newTask: "",
+      // 4 arrays to keep track of our 4 statuses
+      arrBackLog: [
+        { name: "Code Sign Up Page" },
+        { name: "Test Dashboard" },
+        { name: "Style Registration" },
+        { name: "Help with Designs" }
+      ],
+      arrInProgress: [],
+      arrTested: [],
+      arrDone: []
+    };
+  },
+  methods: {
+    //add new tasks method
+    add: function() {
+      if (this.newTask) {
+        this.arrBackLog.push({ name: this.newTask });
+        this.newTask = "";
+      }
+    }
+  }
 };
 </script>
+
+<style>
+/* light stylings for the kanban columns */
+.kanban-column {
+  min-height: 300px;
+}
+</style>
