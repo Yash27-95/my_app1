0ce341cc3734f952754b1c3e584a2a23a6af764c
Yash27-95 <70720726+Yash27-95@users.noreply.github.com>
2020-12-03 18:08:15 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+
+// Import Bootstrap Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
-  vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
